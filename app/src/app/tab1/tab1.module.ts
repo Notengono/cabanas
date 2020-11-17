7a486dd9e37eb0e522114245326918095aa83454
Notengono <remedi.martin@gmail.com>
2020-11-17 16:05:39 -0300

    Mostrando el calendario

--- app/src/app/tab1/tab1.module.ts
+++ app/src/app/tab1/tab1.module.ts
@@ -1,22 +1,23 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from "@ionic/angular";
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Tab1Page } from "./tab1.page";
+import { ExploreContainerComponentModule } from "../explore-container/explore-container.module";
 
-import { Tab1PageRoutingModule } from './tab1-routing.module';
-import { NgCalendarModule } from 'ionic2-calendar';
+import { Tab1PageRoutingModule } from "./tab1-routing.module";
+import { NgCalendarModule } from "ionic2-calendar";
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule,
-    NgCalendarModule
-  ],
-  declarations: [Tab1Page]
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        ExploreContainerComponentModule,
+        Tab1PageRoutingModule,
+        NgCalendarModule,
+    ],
+    providers: [{ provide: LOCALE_ID, useValue: "es-AR" }],
+    declarations: [Tab1Page],
 })
 export class Tab1PageModule {}
--- app/src/app/tab1/tab1.page.ts
+++ app/src/app/tab1/tab1.page.ts
@@ -3,6 +3,10 @@
 import { Component, ViewChild } from "@angular/core";
 import { CalendarComponent } from "ionic2-calendar";
 
+import { registerLocaleData } from "@angular/common";
+import localeEs from "@angular/common/locales/es-AR";
+registerLocaleData(localeEs);
+
 @Component({
     selector: "app-tab1",
     templateUrl: "tab1.page.html",
@@ -23,12 +27,107 @@ export class Tab1Page {
     next() {
         this.myCal.slideNext();
     }
-    
+
     back() {
         this.myCal.slidePrev();
     }
 
     onViewTitleChanged(title) {
         this.viewTitle = title;
+    }
+
+    createRandomEvents() {
+        var events = [];
+        // for (var i = 0; i < 50; i += 1) {
+        //   var date = new Date();
+        //   var eventType = Math.floor(Math.random() * 2);
+        //   var startDay = Math.floor(Math.random() * 90) - 45;
+        //   var endDay = Math.floor(Math.random() * 2) + startDay;
+        //   var startTime;
+        //   var endTime;
+        //   if (eventType === 0) {
+        //     startTime = new Date(
+        //       Date.UTC(
+        //         date.getUTCFullYear(),
+        //         date.getUTCMonth(),
+        //         date.getUTCDate() + startDay
+        //       )
+        //     );
+        //     if (endDay === startDay) {
+        //       endDay += 1;
+        //     }
+        //     endTime = new Date(
+        //       Date.UTC(
+        //         date.getUTCFullYear(),
+        //         date.getUTCMonth(),
+        //         date.getUTCDate() + endDay
+        //       )
+        //     );
+        //     events.push({
+        //       title: 'All Day - ' + i,
+        //       startTime: startTime,
+        //       endTime: endTime,
+        //       allDay: true,
+        //     });
+        //   } else {
+        //     var startMinute = Math.floor(Math.random() * 24 * 60);
+        //     var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        //     startTime = new Date(
+        //       date.getFullYear(),
+        //       date.getMonth(),
+        //       date.getDate() + startDay,
+        //       0,
+        //       date.getMinutes() + startMinute
+        //     );
+        //     endTime = new Date(
+        //       date.getFullYear(),
+        //       date.getMonth(),
+        //       date.getDate() + endDay,
+        //       0,
+        //       date.getMinutes() + endMinute
+        //     );
+        //     console.info(startTime, endTime)
+        //     events.push({
+        //       title: 'Event - ' + i,
+        //       startTime: startTime,
+        //       endTime: endTime,
+        //       allDay: false,
+        //     });
+        //   }
+        // }
+        var startTime;
+        var endTime;
+        var date = new Date();
+        // var startDay = 17;
+        // var endDay = 18;
+
+        startTime = new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate(),
+            13, 0, 0            
+            );
+
+            console.log(startTime)
+            console.log(date.getMinutes())
+
+        endTime = new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate() + 4,
+            10
+        );
+
+        events.push({
+            title: "Alquiler - 1",
+            startTime: startTime,
+            endTime: endTime,
+            allDay: false,
+        });
+        this.eventSource = events;
+    }
+
+    removeEvents() {
+        this.eventSource = [];
     }
 }
